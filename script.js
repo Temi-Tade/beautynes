d95ee1ebc643b5ff71939b5b15328e7e2b1e48a2
heesak <adesuyiojo04@gmail.com>
2024-01-21 02:31:39 +0100

    homepage done

---
+++ script.js
@@ -0,0 +1,13 @@
+var show = document.querySelector('.bar')
+var men =document.querySelector('.mobile-ul')
+var over =document.querySelector('.overlay')
+
+
+
+function addClass(){
+    show.classList.toggle("me")
+    men.classList.toggle("hh")
+    over.classList.toggle("hhh")
+
+
+}
