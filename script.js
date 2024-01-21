07ae7159cce54802c356caef57ae21344cdf8aed
Temi-Tade <akintadetemi19@gmail.com>
2024-01-21 19:21:04 +0100

    fixed some issues

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var show = document.querySelector('.bar')
 var men =document.querySelector('.mobile-ul')
 var over =document.querySelector('.overlay')
 
+function addClass(icon){
+    if (icon.getAttribute("class") === "fa-solid fa-bars") {
+        icon.setAttribute("class", "fa-solid fa-times")
+    } else {
+        icon.setAttribute("class", "fa-solid fa-bars")
+    }
 
-
-function addClass(){
-    show.classList.toggle("me")
     men.classList.toggle("hh")
     over.classList.toggle("hhh")
-
-
 }
